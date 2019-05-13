aabfbf6e232ef00ce49caee7d9e9b1215f934434
Andy Lu <andy@stitchdata.com>
2019-05-13 19:32:36 +0000

    Pin dependencies

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(name='tap-appfigures',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_appfigures'],
       install_requires=[
-          'singer-python',
-          'requests',
+          'singer-python==5.5.1',
+          'requests==2.20.1',
       ],
       entry_points='''
           [console_scripts]
