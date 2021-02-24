a544d122061867bfdbd9a1dd1387459438a68bd2
Grayson Williams <67605231+graysonwilliams-elevate@users.noreply.github.com>
2021-02-24 15:55:39 -0600

    foo

--- tap_appfigures/streams/base.py
+++ tap_appfigures/streams/base.py
@@ -31,7 +31,7 @@ class AppFiguresBase:
 
     def __init__(self, client, state, catalog):
         self.schema = None
-
+        print(catalog)
         if catalog:
             stream_details = stream_details_from_catalog(catalog, self.STREAM_NAME)
             if stream_details:
